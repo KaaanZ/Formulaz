39f2064e1c6b8f566e169ab0142ece64cbbeba88
KanZ <105295347+KaaanZ@users.noreply.github.com>
2022-05-11 13:24:21 +0200

    Add files via upload

---
+++ script.js
@@ -0,0 +1,10 @@
+function sendmail()
+{
+ let mail = document.getElementById('usersmail').value
+ let name = document.getElementById('name').value
+ let surename = document.getElementById('surename').value
+ let age = document.getElementById('age').value
+ console.log(mail + name + surename + age)
+
+ let wynik = document.getElementById('wynik').textContent
+}
