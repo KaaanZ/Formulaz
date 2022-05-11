f189048cf0ab03f811f7c0b88abd636d083803e9
KanZ <105295347+KaaanZ@users.noreply.github.com>
2022-05-11 18:18:28 +0200

    Add files via upload

--- script.js
+++ script.js
@@ -1,10 +1,24 @@
 function sendmail()
 {
- let mail = document.getElementById('usersmail').value
- let name = document.getElementById('name').value
- let surename = document.getElementById('surename').value
- let age = document.getElementById('age').value
- console.log(mail + name + surename + age)
+    let mail = document.getElementById('usersmail').value
+    let name = document.getElementById('name').value
+    let surename = document.getElementById('surename').value
+    let age = document.getElementById('age').value
+    console.log(mail + name + surename + age)
+    let wynik = document.getElementById('wynik').textContent
 
- let wynik = document.getElementById('wynik').textContent
 }
+
+function agecheck()
+{
+    let age = document.getElementById('age').value
+    if(age >=18)
+    {
+        sendmail();
+    }
+    else
+    {
+        alert("Masz za mało lat")
+    }
+}
+
